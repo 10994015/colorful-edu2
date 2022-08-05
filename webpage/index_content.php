8532f5f74033ef41f5536bbd5d67abd34b7c21a2
10994015 <a0938599191@gmail.com>
2022-08-05 15:41:40 +0800

    0805

--- webpage/index_content.php
+++ webpage/index_content.php
@@ -1,10 +1,80 @@
-<link rel="stylesheet" type="text/css" href="./shared/slick-1.6.0/slick/slick.css"/>
-<link rel="stylesheet" type="text/css" href="./shared/slick-1.6.0/slick/slick-theme.css"/>
-<div class="responsive">
-    <div><a href="javascript:;"><img src="./images/banner.png" alt=""></a></div>
-    <div><a href="javascript:;"><img src="./images/banner.png" alt=""></a></div>
+
+<div id="index_content">
+    <div class="responsive">
+        <div><a href="javascript:;"><img src="./images/banner.png" alt=""></a></div>
+        <div><a href="javascript:;"><img src="./images/banner.png" alt=""></a></div>
+    </div>
+
+    <div class="course">
+        <div class="title">
+            <span class="line"></span>
+            <h2>COURSE</h2>
+            <span class="line"></span>
+        </div>
+        <p class="small-title">Most Interesting Popular Courses & Upcoming Courses & Current Courses</p>
+        <div class="courseList">
+            <a href="javascript:;" class="courseItem">
+                <img src="./images/a.jpg" alt="">
+                <h3>Bingfen Culture and Education Science Experiment ~ Do it yourself</h3>
+            </a>
+            <a href="javascript:;" class="courseItem">
+                <img src="./images/b.jpg" alt="">
+                <h3>Bingfen Culture and Education Science Experiment ~ Do it yourself<br>Bingfen Culture and Education Science Experiment ~ Do it yourself</h3>
+            </a>
+            <a href="javascript:;" class="courseItem">
+                <img src="./images/c.jpg" alt="">
+                <h3>Bingfen Culture and Education Science Experiment ~ Do it yourself</h3>
+            </a>
+        </div>
+    </div>
+
+    <div class="news">
+        <div class="title">
+            <span class="line"></span>
+            <h2>NEWS</h2>
+            <span class="line"></span>
+        </div>
+        <p class="small-title"></p>
+        <div class="newsList">
+            <?php for($i=0;$i<=4;$i++){ ?>
+            <div class="newsItem">
+                <img src="./images/b.jpg" alt="">
+                <div class="text-content">
+                    <h3>Colorful Education wishes you a Happy Father's Day</h3>
+                    <p>冰芬文教祝全天下偉大的媽媽們
+                        母親節快樂 Happy Mother’s Day❤️❤️
+                        小編腦中出現這首歌，世上只有媽媽好，有媽的孩子像個寶😍
+
+                        另外昨日第一次進行分流的說明會圓滿落幕了
+                        冰芬也特別準備了康乃馨給媽咪們🥰
+                        希望都有收到我們的小小祝福💖
+
+                        康乃馨 Carnation 神聖之花
+                        花語：聖潔或慈愛之意，被視為對母親心存敬意！
+
+                        ‼️最後一場‼️課程體驗說明會
+                        5/21星期六 下午1:30
+                        歡迎大家來共襄盛舉🤩還可以參加抽獎喔🎁
+                        諮詢電話：03-567-0018
+                        ↓說明會報名連結↓
+                        https://forms.gle/9dpN9VtP3pRL4KdWA
+
+                        ================================
+
+                        📩歡迎臉書粉絲團私訊詢問
+                        📩電子信箱：service@ice-finland.pro
+                        📩官方line：@516ttumg
+                        📩冰芬官網：https://www.ice-finland.club</p>
+                    <a href="javascript:;"><div class="back"></div>Check it out Here »</a>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
 </div>
 
+<link rel="stylesheet" type="text/css" href="./shared/slick-1.6.0/slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="./shared/slick-1.6.0/slick/slick-theme.css"/>
 <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript" src="./shared/slick-1.6.0/slick/slick.min.js"></script>
