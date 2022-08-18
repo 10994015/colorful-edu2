136a3aba6f4510bb97a96f14e92c4148d41975f8
10994015 <a0938599191@gmail.com>
2022-08-19 02:36:45 +0800

    0819

---
+++ webpage/about.php
@@ -0,0 +1,52 @@
+<div id="about">
+    <div class="coverBox">
+        <img src="./images/about.jpg" class="coverImg">
+        <h3>ABOUT US</h3>
+    </div>
+    <div class="aboutContent">
+        <img src="./images/a.jpg">
+        <div class="aboutText">
+            <h4>歡迎來到冰芬文教</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint itaque dolore ratione ut a in atque magnam laborum harum tenetur maiores, ducimus totam, quis quidem culpa quasi fugit. Eius?
+            Eos facere quae iure alias libero laboriosam. Eligendi dicta nulla quis nam ut quaerat distinctio necessitatibus est quam! Laboriosam odio facere dignissimos voluptas doloribus recusandae veritatis officia sed obcaecati magni!
+            </p>
+        </div>
+    </div>
+    <div class="box">
+        <div class="item">
+            <i class="fa-solid fa-phone"></i>
+            <h2>來電詢問</h2>
+            <p>
+                冰芬美語文理補習班<br />
+                300新竹市東區光復路一段271號3樓(原聯合補習班址、台新銀行樓上)<br />
+                連絡電話：03-567-0018
+            </p>
+            <span>03-567-0018</span>
+        </div>
+        <div class="item">
+            <i class="fa-solid fa-comments"></i>
+            <h2>線上詢問</h2>
+            <p>歡迎於以下官方臉書專頁、Instagram及line@訊息詢問更多優惠課程及相關資訊內容喔！</p>
+            <div>
+                <a target="_blank" href="https://www.facebook.com/icefinland/" class="icon"><i class="fab fa-facebook-square"></i></a>
+                <a target="_blank" href="https://www.instagram.com/colorful.institute/" class="icon"><i class="fab fa-instagram"></i></a>
+                <a target="_blank" href="https://lin.ee/7TPK9Fd" class="icon"><i class="fab fa-line"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="contact">
+        <form action="./ref/send.php" method="post">
+            <h2>有什麼話想說?</h2>
+            <span>你的全名</span>
+            <input type="text" required name="name">
+            <span>你的電子郵件</span>
+            <input type="email" required name="email">
+            <span>主旨</span>
+            <input type="text" required name="title">
+            <span>你的訊息(選填)</span>
+            <textarea name="content" id="" cols="30" rows="10" placeholder="報名課程、場地租借、進行培育..."></textarea>
+            <button type="submit">送出</button>
+        </form>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3622.386173646672!2d121.01740541483659!3d24.78222678409088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3468363ae7276ba5%3A0x4e22c2db67d6fe92!2zMzAw5paw56u55biC5p2x5Y2A5YWJ5b6p6Lev5LiA5q61Mjcx6JmfM-aokw!5e0!3m2!1szh-TW!2stw!4v1645111991019!5m2!1szh-TW!2stw" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+</div>
