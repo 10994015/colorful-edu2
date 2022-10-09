33e2d2147ba6f2d5d22591ada1649aaa68664413
10994015 <a0938599191@gmail.com>
2022-10-10 00:02:25 +0800

    delete RWD

--- cms/index.php
+++ cms/index.php
@@ -24,7 +24,7 @@ if(isset($_SESSION['username'])){
 <html lang="zh-Hant-TW">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, minimum-scale=1.0, maximum-scale=3.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, minimum-scale=1.0, maximum-scale=3.0"> -->
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="robots" content="noindex">
     <title>後台管理系統</title>
--- cms/updateCourse.php
+++ cms/updateCourse.php
@@ -22,7 +22,7 @@ if(isset($_SESSION['username'])){
 <html lang="zh-Hant-TW">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, minimum-scale=1.0, maximum-scale=3.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, minimum-scale=1.0, maximum-scale=3.0"> -->
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>後台管理系統</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
